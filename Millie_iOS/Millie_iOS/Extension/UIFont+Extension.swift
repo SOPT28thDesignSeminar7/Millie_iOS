627c98e2926325a43c1e13b265ad17f1c0eff094
inaeLee <inae9709@hanmail.net>
2021-05-17 22:12:27 +0900

    add: NotoSans Font + Extension 추가

--- Millie_iOS/Millie_iOS/Extension/UIFont+Extension.swift
+++ Millie_iOS/Millie_iOS/Extension/UIFont+Extension.swift
@@ -7,4 +7,21 @@
 
 import UIKit
 
-extension UIFont {}
+extension UIFont {
+    class func NotoSansKR(type: NotoSansKRType, size: CGFloat) -> UIFont! {
+        guard let font = UIFont(name: type.name, size: size) else {
+            return nil
+        }
+        return font
+    }
+
+    public enum NotoSansKRType: String {
+        case bold = "Bold"
+        case medium = "Medium"
+        case regular = "Regular"
+
+        var name: String {
+            "NotoSansKR-" + self.rawValue
+        }
+    }
+}
