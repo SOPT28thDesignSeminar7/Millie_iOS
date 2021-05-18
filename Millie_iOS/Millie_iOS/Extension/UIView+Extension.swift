e37f31afd47a6992616588c829af62809485cff8
inaeLee <inae9709@hanmail.net>
2021-05-19 02:12:11 +0900

    add: shadow, round extension 함수  추가

--- Millie_iOS/Millie_iOS/Extension/UIView+Extension.swift
+++ Millie_iOS/Millie_iOS/Extension/UIView+Extension.swift
@@ -13,4 +13,26 @@ extension UIView {
             self.addSubview(view)
         }
     }
+
+    func setShadow(radius: CGFloat, offset: CGSize, opacity: Float, color: UIColor) {
+        self.layer.shadowOpacity = opacity
+        self.layer.shadowOffset = offset
+        self.layer.shadowRadius = radius
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = color.cgColor
+        self.layer.shouldRasterize = true
+
+        self.layer.shouldRasterize = true
+        self.layer.rasterizationScale = UIScreen.main.scale
+    }
+
+    func cornerRounds() {
+        self.layer.cornerRadius = self.layer.frame.height / 2
+        self.layer.masksToBounds = true
+    }
+
+    func cornerRound(radius: CGFloat) {
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
+    }
 }
---
+++ Millie_iOS/Millie_iOS/Inae/Cell/InaeBookCell.swift
@@ -0,0 +1,23 @@
+//
+//  InaeBookCell.swift
+//  Millie_iOS
+//
+//  Created by inae Lee on 2021/05/19.
+//
+
+import UIKit
+
+class InaeBookCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
