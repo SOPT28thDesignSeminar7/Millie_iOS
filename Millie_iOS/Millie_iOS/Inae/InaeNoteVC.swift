950dfbb655c341cb766058dde081103820ac43cb
inaeLee <inae9709@hanmail.net>
2021-05-18 23:09:15 +0900

    add: header Button 생성 (레이아웃 만들기)

--- Millie_iOS/Millie_iOS/Inae/InaeNoteVC.swift
+++ Millie_iOS/Millie_iOS/Inae/InaeNoteVC.swift
@@ -8,12 +8,6 @@
 import SnapKit
 import UIKit
 
-enum Menu: String {
-    case myBookCase = "내책장"
-    case readingNote = "독서노트"
-    case statistics = "통계"
-}
-
 class InaeNoteVC: UIViewController {
     // MARK: - IBOutlets
 
@@ -24,9 +18,68 @@ class InaeNoteVC: UIViewController {
     @IBOutlet var goalLabel: UILabel!
     @IBOutlet var tableView: UITableView!
 
-    // MARK: - Local variables
+    // MARK: - UIComponents
 
-    let menuItems = [Menu.myBookCase, Menu.readingNote, Menu.statistics]
+    private lazy var headerView: UIView = {
+        let view = UIView()
+        let bookmark = UIImageView(image: UIImage(named: "btnFat"))
+
+        view.addSubviews([myBookCaseButton, readingNoteButton, statisticsButton, bookmark])
+        myBookCaseButton.snp.makeConstraints { make in
+            make.top.equalTo(18)
+            make.leading.equalToSuperview().inset(14)
+            make.bottom.equalToSuperview().inset(6)
+        }
+
+        readingNoteButton.snp.makeConstraints { make in
+            make.top.equalTo(18)
+            make.leading.equalTo(myBookCaseButton.snp.trailing).inset(-27)
+            make.bottom.equalToSuperview().inset(6)
+        }
+
+        statisticsButton.snp.makeConstraints { make in
+            make.top.equalTo(18)
+            make.leading.equalTo(readingNoteButton.snp.trailing).inset(-27)
+            make.bottom.equalToSuperview().inset(6)
+        }
+
+        bookmark.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(-2)
+            make.trailing.equalToSuperview().inset(15)
+            make.height.equalTo(42)
+        }
+
+        return view
+    }()
+
+    private lazy var myBookCaseButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("내책장", for: .normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        btn.titleLabel?.font = UIFont.NotoSansKR(type: .regular, size: 15)
+
+        return btn
+    }()
+
+    private lazy var readingNoteButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("독서노트", for: .normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        btn.titleLabel?.font = UIFont.NotoSansKR(type: .regular, size: 15)
+
+        return btn
+    }()
+
+    private lazy var statisticsButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("통계", for: .normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        btn.titleLabel?.font = UIFont.NotoSansKR(type: .regular, size: 15)
+
+        return btn
+    }()
+
+    // MARK: - Local variables
 
     // MARK: - LifeCycle Methods
 
@@ -58,16 +111,27 @@ extension InaeNoteVC: UITableViewDataSource {
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: InaeTagCell.identifier) as? InaeTagCell else { return UITableViewCell() }
+        switch indexPath.row {
+        case 0:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: InaeTagCell.identifier) as? InaeTagCell else { return UITableViewCell() }
 
-        return cell
+            return cell
+        default:
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: InaeTagCell.identifier) as? InaeTagCell else { return UITableViewCell() }
+
+            return cell
+        }
     }
 }
 
 // MARK: - UITableViewDelegate
 
 extension InaeNoteVC: UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        UIView()
-//    }
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        headerView
+    }
+
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        48
+    }
 }
