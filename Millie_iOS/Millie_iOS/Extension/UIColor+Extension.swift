45c3f95d0ef761ec89ebd4a81be4f0d8f60b0f13
inaeLee <inae9709@hanmail.net>
2021-05-19 14:10:09 +0900

    add: UIColor Extension 추가

---
+++ Millie_iOS/Millie_iOS/Extension/UIColor+Extension.swift
@@ -0,0 +1,50 @@
+//
+//  UIColor+Extension.swift
+//  Millie_iOS
+//
+//  Created by inae Lee on 2021/05/19.
+//
+
+import UIKit
+
+extension UIColor {
+    @nonobjc class var fontMainColor: UIColor {
+        UIColor(red: 85 / 255, green: 85 / 255, blue: 85 / 255, alpha: 1)
+    }
+
+    @nonobjc class var fontLightGrayColor: UIColor {
+        UIColor(red: 241 / 255, green: 241 / 255, blue: 241 / 255, alpha: 1)
+    }
+
+    @nonobjc class var grayTextColor: UIColor {
+        UIColor(red: 134 / 255, green: 141 / 255, blue: 155 / 255, alpha: 1)
+    }
+
+    @nonobjc class var tabbarTabbarYellow: UIColor {
+        UIColor(red: 245.0 / 255.0, green: 194.0 / 255.0, blue: 67.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var tabbarTabbarGray: UIColor {
+        UIColor(white: 147.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var fontHighlight: UIColor {
+        UIColor(red: 80.0 / 255.0, green: 65.0 / 255.0, blue: 98.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var fontEmphasis: UIColor {
+        UIColor(white: 36.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var veryLightPink: UIColor {
+        UIColor(red: 245.0 / 255.0, green: 244.0 / 255.0, blue: 243.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var lightPurple: UIColor {
+        UIColor(red: 171.0 / 255.0, green: 114.0 / 255.0, blue: 241.0 / 255.0, alpha: 1.0)
+    }
+
+    @nonobjc class var black26: UIColor {
+        UIColor(white: 0.0, alpha: 0.26)
+    }
+}
