b366e6cba017258d165bc7cf7e41f0915c4a66ea
inaeLee <inae9709@hanmail.net>
2021-05-19 14:03:05 +0900

    add: NotoSerif Font Extension 추가

--- Millie_iOS/Millie_iOS/Extension/UIFont+Extension.swift
+++ Millie_iOS/Millie_iOS/Extension/UIFont+Extension.swift
@@ -34,9 +34,25 @@ extension UIFont {
 
     public enum LatoType: String {
         case regular = "Regular"
+        case bold = "Bold"
 
         var name: String {
             "Lato-" + self.rawValue
+        }
+    }
+
+    class func NotoSerifKR(type: NotoSerifKRType, size: CGFloat) -> UIFont! {
+        guard let font = UIFont(name: type.name, size: size) else {
+            return nil
+        }
+        return font
+    }
+
+    public enum NotoSerifKRType: String {
+        case medium = "Medium"
+
+        var name: String {
+            "NotoSerifKR-" + self.rawValue
         }
     }
 }
